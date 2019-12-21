a5ed22a34297a14466a919bffec2b700a7511150
Rudy Dahbura <rdahbura@gmail.com>
2019-12-20 20:20:29 -0800

    Fixed callback.

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,4 +1,5 @@
 const AUTH0_CALLBACK_URL = process.env.AUTH0_CALLBACK_URL || '';
+const AUTH0_CALLBACK_POPUP_URL = process.env.AUTH0_CALLBACK_POPUP_URL || '';
 const AUTH0_CLIENT_ID = process.env.AUTH0_CLIENT_ID || '';
 const AUTH0_CLIENT_SECRET = process.env.AUTH0_CLIENT_SECRET || '';
 const AUTH0_DOMAIN = process.env.AUTH0_DOMAIN || '';
@@ -12,6 +13,7 @@ const isProduction = (): boolean => {
 
 export {
   AUTH0_CALLBACK_URL,
+  AUTH0_CALLBACK_POPUP_URL,
   AUTH0_CLIENT_ID,
   AUTH0_CLIENT_SECRET,
   AUTH0_DOMAIN,
